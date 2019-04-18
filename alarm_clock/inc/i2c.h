1099a2eaac40c011395920e9bf7c38c0ca3c10c0
nate <nate.givens03@gmail.com>
2019-04-17 21:30:17 -0400

    adding RTC interfacing

---
+++ alarm_clock/inc/i2c.h
@@ -0,0 +1,33 @@
+#include "stm32f0xx.h"
+#include "stm32f0_discovery.h"
+#include "rtc.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+#define FAIL -1
+#define SUCCESS 0
+#define WR 0
+#define RD 1
+
+
+void I2C1_waitidle(void);
+void init_I2C1();
+void I2C1_start(uint8_t addr, uint32_t dir);
+void I2C1_stop();
+int I2C1_senddata(uint8_t* data, uint32_t size);
+int I2C1_readdata(uint8_t* data, uint32_t size);
+void read_time(uint8_t *);
+void set_time();
+void init_DS3231();
+
+/*
+inline uint8_t dec_to_bcd(uint8_t dec) {
+  return ((((dec / 10) & 0xF) << 4) |
+           ((dec % 10) & 0xF));
+}
+*/
+
+inline uint8_t bcd_to_dec(uint8_t bcd){
+    return ((bcd >> 4) * 10) + (bcd & 0x0F);
+}
---
+++ alarm_clock/inc/rtc.h
@@ -0,0 +1,33 @@
+#include "stm32f0xx.h"
+#include "stm32f0_discovery.h"
+#include <stdio.h>
+#include <string.h>
+
+#define ADDR_SECONDS 0x00
+#define ADDR_MINUTES 0x01
+#define ADDR_HOURS   0x02
+#define ADDR_DAY     0x03
+#define ADDR_DATE    0x04
+#define ADDR_MONTH   0x05
+#define ADDR_YEAR    0x06
+
+#define ADDR_CONTROL 0x0E
+#define ADDR_STATUS  0x0F
+
+#define ADDR_TEMP    0x11
+
+#define ADDR_RTC_I2C 0x68
+ //or 0xD0?
+
+struct Time{
+    uint8_t secs;
+    uint8_t mins;
+    uint8_t hours;
+
+};
+
+struct Date{
+    uint8_t day;
+    uint8_t month;
+    uint8_t year;
+};
